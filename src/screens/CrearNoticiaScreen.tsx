4fd352035c9e5de40ebd75959593a5eeea3b4352
Ricardo Torres <ricardo.torresits@gmail.com>
2022-10-05 12:23:38 -0500

    camara y libreria

--- src/screens/CrearNoticiaScreen.tsx
+++ src/screens/CrearNoticiaScreen.tsx
@@ -1,27 +1,108 @@
 import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from '../theme/CrearNoticiaTheme';
 
 export const CrearNoticiaScreen = () => {
     const {top} = useSafeAreaInsets()
+
+    const takePhoto = () => {
+        launchCamera({
+          mediaType:'photo',
+          quality:0.5
+        }, (resp:any) =>{
+          if (resp.didCancel) return
+          if(!resp.assets[0].uri) return
+        });
+      }
+
+      const takePhotoFromGalery = () => {
+        launchImageLibrary({
+          mediaType:'photo',
+          quality:0.5
+        }, (resp:any) =>{
+          if (resp.didCancel) return
+          if(!resp.assets[0].uri) return
+        });
+      }
+
   return (
-    <View style={{...styles.containerMaster,top:top}}>
+    <View style={[styles.containerMaster,top>0?{top:top+5}:{top:35}]}>
         <Text style={styles.textContainer}>
             Crear noticia
         </Text>
         <View
             style={styles.container}
         >
-            <Image
-                source={require('../assets/alex-suprun-ZHvM3XIOHoE-unsplash.jpg')}
-                style={styles.UserImg}
-            />
-            <Text
-                style={styles.textUser}
-            >
-                Nico
-            </Text>
+            <View style={styles.containerUser}>
+                <Image
+                    source={require('../assets/alex-suprun-ZHvM3XIOHoE-unsplash.jpg')}
+                    style={styles.UserImg}
+                />
+                <Text
+                    style={styles.textUser}
+                >
+                    Nico
+                </Text>
+            </View>
+            <View style={{width:'100%',marginTop:'4%',height:.5,backgroundColor:'#656565'}}/>
+            <ScrollView style={styles.ContianerPublicaion}>
+                <Text style={styles.title}>
+                    Descripción
+                </Text>
+                <TextInput
+                    placeholder='¿Qué quieres contar?'
+                    placeholderTextColor="#838383"
+                    style={styles.textInput}
+                    multiline={true}
+                    textAlign={'left'}
+                    maxLength={10000}
+                    onChangeText={(value)=> {}}
+                    //value={}
+                    //onSubmitEditing={}
+                />
+                <Image/>
+            </ScrollView>
+            <View style={{width:'100%',marginTop:'3%',height:.5,backgroundColor:'#656565'}}/>
+            <View style={styles.containerMasterButton}>
+                <View style={styles.ContainerButtonCamara}>
+                    <TouchableOpacity style={styles.ButtonCamaraGaleria}
+                        onPress={takePhoto}
+                    >
+                        <Image
+                            source={require('../assets/Camara.png')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.ButtonCamaraGaleria}
+                        onPress={takePhotoFromGalery}
+                    >
+                        <Image
+                            source={require('../assets/Galeria.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.containerButon}>
+                    
+                    <TouchableOpacity style={{...styles.Button,
+                            backgroundColor:'#FFFFFF',
+                            borderColor:'#C7CCDC',
+                            borderWidth:1
+                        }}
+                        onPress={takePhoto}    
+                    >
+                        <Text style={{...styles.ButtonText,color:'#093C5D'}}>
+                            Cancelar
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{...styles.Button,backgroundColor:'#FABB00'}}>
+                        <Text style={{...styles.ButtonText,color:'#FFFFFF'}}>
+                            Publicar
+                        </Text>
+                    </TouchableOpacity>
+
+                </View>
+            </View>
         </View>
     </View>
   )
--- src/theme/CrearNoticiaTheme.tsx
+++ src/theme/CrearNoticiaTheme.tsx
@@ -22,8 +22,14 @@ export const styles = StyleSheet.create({
         width:'100%',
         borderTopStartRadius:50,
         borderTopEndRadius:50,
-        alignItems:'center',
         marginTop:10
+    },
+    containerUser:{
+        backgroundColor:'#FFFFFF',
+        width:'100%',
+        alignItems:'center',
+        borderTopStartRadius:50,
+        borderTopEndRadius:50,
     },
     UserImg:{
         width:47,
@@ -37,5 +43,60 @@ export const styles = StyleSheet.create({
         fontSize:16,
         lineHeight:18.75,
         marginTop:10
+    },
+    ContianerPublicaion:{
+        paddingHorizontal:'15%',
+        marginTop:'5%'
+    },
+    title:{
+        color:'#093C5D',
+        fontSize:16.67,
+        lineHeight:19.53,
+        fontWeight:'500'
+    },
+    textInput:{
+        backgroundColor:'#FFFFFF',
+        width:'100%'
+    },
+    containerMasterButton:{
+        height:'30%',
+        width:'100%'
+    },
+    ContainerButtonCamara:{
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:'10%',
+        height:'5%'
+    },
+    ButtonCamaraGaleria:{
+        backgroundColor:'white',
+        borderColor:'#D8D8D8',
+        width:47,
+        height:47,
+        borderRadius:10,
+        borderWidth:1.04,
+        justifyContent:'center',
+        alignItems:'center',
+        marginHorizontal:'5%'
+    },
+    containerButon:{
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:'10%'
+    },
+    Button:{
+        justifyContent:'center',
+        alignItems:'center',
+        height:'60%',
+        width:'30%',
+        borderRadius:21,
+        marginHorizontal:'3%'
+    },
+    ButtonText:{
+        fontSize:17,
+        fontWeight:'700',
+        lineHeight:20
     }
 });
