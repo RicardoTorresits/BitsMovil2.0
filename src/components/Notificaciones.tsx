9c366ec4406495b830d22589d4381f06c7e83c98
Ricardo Torres <ricardo.torresits@gmail.com>
2022-10-07 10:51:17 -0500

    respaldo para las notificaciones

---
+++ src/components/Notificaciones.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-animatable'
+import { styles } from '../theme/NotificacionesTheme'
+
+
+export const Notificaciones = () => {
+  return (
+    <View
+        style={{
+            alignItems:'center',
+            alignContent:'center',
+            paddingHorizontal:20
+        }}
+    >
+        <View
+            style={styles.container}
+        >
+            <View>
+                <View>
+
+                </View>
+                <View>
+                    
+                </View>
+                <View>
+                    
+                </View>
+            </View>
+        </View>
+    </View>
+  )
+}
