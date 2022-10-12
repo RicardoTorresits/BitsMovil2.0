b58e7345e3fe51c018cdf9b1a156aa0c86aa1527
Ricardo Torres <ricardo.torresits@gmail.com>
2022-10-12 15:10:03 -0500

    prueba 4

--- src/screens/CrearNoticiaScreen.tsx
+++ src/screens/CrearNoticiaScreen.tsx
@@ -45,7 +45,6 @@ export const CrearNoticiaScreen = ({navigation}:Props) => {
             name:resp.assets[0].fileName,
             type:'image/png'
           }
-          console.log(file)
           const option ={
             keyPrefix:'ArchivoRespuesta /',
             bucket:'bits-qa-datos-candidatos',
@@ -56,7 +55,7 @@ export const CrearNoticiaScreen = ({navigation}:Props) => {
           }
           RNS3.put(file,option)
           .then((response)=>{
-            setAws(response.body.postResponse.location.uri)
+            setAws(response.body.postResponse.location)
             console.log(aws)
           })
           settempUri(resp.assets[0].uri)
@@ -75,7 +74,6 @@ export const CrearNoticiaScreen = ({navigation}:Props) => {
             name:resp.assets[0].fileName,
             type:'image/png'
           }
-          console.log(file)
           const option ={
             keyPrefix:'ArchivoRespuesta /',
             bucket:'bits-qa-datos-candidatos',
@@ -86,7 +84,7 @@ export const CrearNoticiaScreen = ({navigation}:Props) => {
           }
           RNS3.put(file,option)
           .then((response)=>{
-            setAws(response.body.postResponse.location.uri)
+            setAws(response.body.postResponse.location)
             console.log(aws)
           })
           settempUri(resp.assets[0].uri)
