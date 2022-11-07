b555ed050427f0d07614a0dbe970f1903b5fb435
RicardoTorresits <ricardo.torresits@gmail.com>
2022-11-07 15:56:26 -0600

    arrelgo para pods e inicio de reacciones

--- src/screens/ReaccionesScreen.tsx
+++ src/screens/ReaccionesScreen.tsx
@@ -1,12 +1,35 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { HeaderCustom } from '../components/HeaderCustom';
+import { styles } from '../theme/ReaccionesTheme';
 
-export const ReaccionesScreen = () => {
+interface Props extends StackScreenProps<any, any> {};
+
+export const ReaccionesScreen = ({navigation,route}:Props) => {
+
+  const {top} = useSafeAreaInsets()
+
   return (
-    <View>
-        <Text>
-        ReaccionesScreen
-        </Text>
+    <View style={{top:top}}>
+        <HeaderCustom navigation={navigation} route={route}/>
+        <View style={styles.container}>
+          <View style={styles.containerReacciones}>
+            <Text>
+              Reacciones
+            </Text>
+            <View style={{flexDirection:'row'}}>
+              <Image source={require('../assets')}/>
+            </View>
+            <View style={{flexDirection:'row'}}>
+
+            </View>
+            <View style={{flexDirection:'row'}}>
+
+            </View>
+          </View>
+        </View>
     </View>
   )
 }
